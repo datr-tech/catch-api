f413351ad31742550a826be3831827a2c26154b4
j-alderson-strachan <j.a.strachan@protonmail.com>
2024-10-13 22:05:25 +0100

    feat: (#7) part complete imagePackFeatureParser - see @TODO tomorrow

--- src/core/parsers/features/imagePack/imagePackFeatureParser.ts
+++ src/core/parsers/features/imagePack/imagePackFeatureParser.ts
@@ -1,10 +1,27 @@
-// import jsdom from 'jsdom';
+import jsdom from 'jsdom';
 import { IParserFeatureImagePack } from '@app/interfaces/core/parsers/features/imagePack';
 
-// const { JSDOM } = jsdom;
+const { JSDOM } = jsdom;
 
-export const imagePackFeatureParser: IParserFeatureImagePack = () => {
-  return {
-    title: 'ABC',
-  };
+export const imagePackFeatureParser: IParserFeatureImagePack = ({ html }) => {
+  try {
+    const { document } = new JSDOM(html).window;
+    const imagePackWrapper = document.getElementsByClassName('Lv2Cle');
+    const hasImagePack = imagePackWrapper.length > 0;
+
+    const imagePackHeaderTitleWrapper = document.getElementsByClassName('T6zPgb YC72Wc');
+    const hasImagePackHeaderTitleWrapper = imagePackHeaderTitleWrapper.length > 0;
+
+    const imagePackSingleImageWrapper = document.getElementsByClassName('div.vCUuC');
+    const hasImagePackSingleImageWrapper = imagePackSingleImageWrapper.length > 0;
+
+    return {
+      hasImagePack,
+      hasImagePackHeaderTitleWrapper,
+      hasImagePackSingleImageWrapper,
+    };
+  } catch (error) {
+    const { message } = error;
+    throw new Error(message);
+  }
 };
